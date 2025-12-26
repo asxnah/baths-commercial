3a8fd84b62f971fb7c94997834b958e2fba1de4f
asuna <cherepanova.so04@gmail.com>
2025-12-26 17:29:46 +0500

    chore: mobile-first

--- app/baths/[id]/page.tsx
+++ app/baths/[id]/page.tsx
@@ -13,29 +13,29 @@ export default async function ProjectPage(props: {
   if (!project) return <p>Проект не найден</p>;
 
   return (
-    <main className="py-4 px-16">
+    <main className="py-4 px-4 sm:px-8 lg:px-16">
       <SeoJsonLd project={project} />
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_512px] gap-8 pb-8">
+        <div className="w-full">
+          <Gallery images={project.images} />
+        </div>
 
-      <div className="grid grid-cols-[1fr_512px] gap-8 pb-8">
-        <Gallery images={project.images} />
-
-        <div className="space-y-6">
-          <h1 className="text-3xl font-bold">{project.title}</h1>
-          <p className="text-3xl font-bold text-green-700">
+        <div className="space-y-6 lg:space-y-8">
+          <h1 className="text-2xl sm:text-3xl font-bold">{project.title}</h1>
+          <p className="text-2xl sm:text-3xl font-bold text-green-700">
             {project.price.toLocaleString("ru-RU")} ₽
           </p>
 
-          <section className="flex flex-col gap-8">
-            <p className="text-gray-700 leading-relaxed">
+          <section className="flex flex-col gap-6 sm:gap-8">
+            <p className="text-base sm:text-lg text-gray-700 leading-relaxed">
               {project.description}
             </p>
             <OrderModal projectTitle={project.title} />
           </section>
         </div>
       </div>
-
-      <section className="my-4">
-        <h2 className="text-2xl font-medium mb-6">Характеристики</h2>
+      <section className="my-8 lg:my-12">
+        <h2 className="text-xl sm:text-2xl font-medium mb-6">Характеристики</h2>
         <SpecsTable specs={project.specs} />
       </section>
     </main>
